2844a2dd76f83563cc4aac87fa6ef36441ba7c9d
Franko-Martin09 <frankomartin23@gmail.com>
2026-04-06 17:16:32 -0300

    feat: implement PUT and DELETE endpoints for Dibujo with basic validation

--- src/main/java/com/artmarketplace/artmarketplace/controller/HolaController.java
+++ src/main/java/com/artmarketplace/artmarketplace/controller/HolaController.java
@@ -12,7 +12,10 @@ import com.artmarketplace.artmarketplace.repository.DibujoRepository;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import java.util.List;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
 /**
  *
  * @author ASUS
@@ -40,7 +43,36 @@ public class HolaController {
         return dibujoRepository.findAll();
     }
     @GetMapping("/dibujos/{id}")
-    public Dibujo obtenerDibujoPorId(@PathVariable Long id){
-    return dibujoRepository.findById(id).orElse(null);
+    public ResponseEntity<Dibujo> obtenerDibujoPorId(@PathVariable Long id){
+       if (dibujoRepository.existsById(id)) {
+           Dibujo dibujo = dibujoRepository.findById(id).orElse(null);
+           return ResponseEntity.ok(dibujo);//200 + el objeto
+      } else {
+        // 3. No existe → error
+        return ResponseEntity.notFound().build(); // 404
     }
+    
+    }
+    @DeleteMapping("/dibujos/{id}")
+    public ResponseEntity<Void> eliminar(@PathVariable Long id) {
+        if(dibujoRepository.existsById(id)){//Verific si existe. 
+            dibujoRepository.deleteById(id);//SI EXISTE->lo borra
+            return ResponseEntity.noContent().build();//204
+        }else {
+            //No existe ->error
+            return ResponseEntity.notFound().build(); //404
+        }
+    }
+    @PutMapping("/dibujos/{id}")
+    public ResponseEntity<Dibujo> actualizarDibujo(@PathVariable Long id,@RequestBody Dibujo dibujoNuevo){
+        if(dibujoRepository.existsById(id)){//Verifica si existe. 
+            dibujoNuevo.setId(id); // el que hay que pisar.
+            Dibujo actualizado = dibujoRepository.save(dibujoNuevo);//save lo actualiza si el id ya existe
+            return ResponseEntity.ok(actualizado);//204 + el dibujo actualizado
+        }else {
+            return ResponseEntity.notFound().build(); //404
+        }
+    }
+        
+    
 }
