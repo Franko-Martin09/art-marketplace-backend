d092b2a8245055d0a69720dcd5a1bbd237f2b6a3
Franko-Martin09 <frankomartin23@gmail.com>
2026-05-20 11:49:07 -0300

    fix: agrega declaracion de package a DibujoDTO

--- src/main/java/com/artmarketplace/artmarketplace/dto/DibujoDTO.java
+++ src/main/java/com/artmarketplace/artmarketplace/dto/DibujoDTO.java
@@ -2,13 +2,13 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-
+package com.artmarketplace.artmarketplace.dto;
 /**
  *
  * @author ASUS
  */
 public class DibujoDTO {
-    public String titulo;
+    private String titulo;
     private double precio;
     
     public String getTitulo(){
